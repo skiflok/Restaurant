c7062e9b3575eccde60050e7bb3803eebce2dc71
skiflok <anisimov.a.v@inbox.ru>
2023-02-02 13:34:57 +0300

    allDishesToString

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,9 +1,12 @@
 package app;
 
 
+import app.kitchen.Dish;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("it restaurant");
+        System.out.println(Dish.allDishesToString());
     }
 }
 

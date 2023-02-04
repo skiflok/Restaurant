6a092ecd8ff36761f950f69a891066869b335426
skiflok <anisimov.a.v@inbox.ru>
2023-02-04 13:37:46 +0300

    tablet createOrder()

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,16 +2,15 @@ package app;
 
 
 
-import app.kitchen.Order;
 
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
         ConsoleHelper.writeMessage("it's a restaurant Restaurant");
-//        ConsoleHelper.getAllDishesForOrder();
-        Order order = new Order(new Tablet(1));
-        ConsoleHelper.writeMessage(order.toString());
+        Tablet tablet1 = new Tablet(1);
+        tablet1.createOrder();
+        ConsoleHelper.writeMessage(tablet1.order.toString());
     }
 }
 
--- src/main/java/app/Tablet.java
+++ src/main/java/app/Tablet.java
@@ -1,15 +1,22 @@
 package app;
 
+import app.kitchen.Order;
+
+import java.io.IOException;
+
 public class Tablet {
     private final int tableNumber;
+    public Order order;
+
+
 
     public Tablet(int tableNumber) {
         this.tableNumber = tableNumber;
     }
 
-//    public void createOrder() {
-//
-//    }
+    public void createOrder() throws IOException {
+        this.order = new Order(this);
+    }
 
     @Override
     public String toString() {

e2b4eb29f1ee149b7b5613d728918210243e07a3
skiflok <anisimov.a.v@inbox.ru>
2023-02-22 10:21:39 +0300

    Dish change add duration

--- src/main/java/app/kitchen/Dish.java
+++ src/main/java/app/kitchen/Dish.java
@@ -1,15 +1,29 @@
 package app.kitchen;
 
 public enum Dish {
-    FISH,
-    STEAK,
-    SOUP,
-    JUICE,
-    WATER;
+    FISH(25),
+    STEAK(30),
+    SOUP(15),
+    JUICE(5),
+    WATER(3);
+
+    private int duration;
+
+    Dish(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 
     public static String allDishesToString() {
         String res = "";
-        for (Dish dish: Dish.values()) {
+        for (Dish dish : Dish.values()) {
             if ("".equals(res)) {
                 res = dish.name();
             } else {

5429bc3cc57f32ec47c41d3862d0d6294972e7cd
skiflok <anisimov.a.v@inbox.ru>
2023-02-24 11:08:52 +0300

    commentaries

--- src/main/java/app/ad/Advertisement.java
+++ src/main/java/app/ad/Advertisement.java
@@ -1,15 +1,28 @@
 package app.ad;
 
+
+/**
+ * рекламное объявление
+ */
 public class Advertisement {
 
     private Object content; // видео
     private String name; //  имя/название
-    private long initialAmount; //  начальная сумма, стоимость рекламы в копейках. Используем long, чтобы избежать проблем с округлением
+    private long initialAmount; //  Начальная сумма, стоимость рекламы в копейках. Используем long, чтобы избежать проблем с округлением
     private int hits; //  количество оплаченных показов
     private int duration; //  продолжительность в секундах
 
     long amountPerOneDisplaying;
 
+
+    /**
+     *
+     * @param content видео
+     * @param name название
+     * @param initialAmount Начальная сумма, стоимость рекламы в копейках
+     * @param hits количество оплаченных показов
+     * @param duration продолжительность в секундах
+     */
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
         this.name = name;

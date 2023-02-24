161b9e0a58d6537b4976a6f318ef6e9d32521ef2
skiflok <anisimov.a.v@inbox.ru>
2023-02-24 12:54:23 +0300

    comparator advertisement and sort

--- src/main/java/app/ad/AdvertisementManager.java
+++ src/main/java/app/ad/AdvertisementManager.java
@@ -2,6 +2,8 @@ package app.ad;
 
 import app.ConsoleHelper;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class AdvertisementManager {
@@ -19,6 +21,17 @@ public class AdvertisementManager {
             throw new NoVideoAvailableException();
         } else {
             List<Advertisement> playlist = storage.list();
+            /*
+            Пример метода сортировки коллекций (List<T>, Comparator<? super T>) [дубликат]
+            https://stackoverflow.com/questions/14154127/collections-sortlistt-comparator-super-t-method-example
+             */
+            Collections.sort(playlist, new Comparator<Advertisement>() {
+                @Override
+                public int compare(Advertisement o1, Advertisement o2) {
+                    long l = o2.getAmountPerOneDisplaying() - o1.getAmountPerOneDisplaying();
+                    return (int) (l != 0 ? l : o2.getDuration() - o1.getDuration());
+                }
+            });
             for (Advertisement video : playlist) {
                 ConsoleHelper.writeMessage(String.format("%s is displaying... %d, %d",
                         video.getName(),

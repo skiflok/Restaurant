5dceffa947eed3d96bff55dae50cc3193b01e044
skiflok <anisimov.a.v@inbox.ru>
2023-02-24 12:18:02 +0300

    processVideos() play video

--- src/main/java/app/ad/AdvertisementManager.java
+++ src/main/java/app/ad/AdvertisementManager.java
@@ -1,5 +1,9 @@
 package app.ad;
 
+import app.ConsoleHelper;
+
+import java.util.List;
+
 public class AdvertisementManager {
 
     private final AdvertisementStorage storage = AdvertisementStorage.getStorage();
@@ -13,6 +17,14 @@ public class AdvertisementManager {
     public void processVideos() {
         if (storage.list().isEmpty()) {
             throw new NoVideoAvailableException();
+        } else {
+            List<Advertisement> playlist = storage.list();
+            for (Advertisement video : playlist) {
+                ConsoleHelper.writeMessage(String.format("%s is displaying... %d, %d",
+                        video.getName(),
+                        video.getAmountPerOneDisplaying(),
+                        video.getAmountPerOneDisplaying() * 1000 / video.getDuration()));
+            }
         }
     }
 }

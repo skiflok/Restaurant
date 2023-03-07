4b30b1dcdf86a6d4e84f28cedbd120ef00e9d8c2
skiflok <anisimov.a.v@inbox.ru>
2023-03-07 13:04:17 +0300

    class StatisticStorage

--- src/main/java/app/statistic/StatisticManager.java
+++ src/main/java/app/statistic/StatisticManager.java
@@ -2,13 +2,22 @@ package app.statistic;
 
 
 import app.statistic.event.EventDataRow;
+import app.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Управляет регистрацией событий и сбором статистики
+ * Управляет регистрацией событий и сбором статистики.
+ * Имеет хранилище в виде внутреннего класса.
  */
 public class StatisticManager {
 
     private static final StatisticManager instance = new StatisticManager();
+    private StatisticStorage statisticStorage = new StatisticStorage();
+
 
     private StatisticManager() {
     }
@@ -20,4 +29,21 @@ public class StatisticManager {
     void register(EventDataRow data) {
         //TODO
     }
+
+
+    private class StatisticStorage {
+
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+
+        /**
+         * Инициализирует хранилище с типами событий и пустым листом
+         */
+        public StatisticStorage() {
+            for (EventType type : EventType.values()) {
+                storage.put(type, new ArrayList<EventDataRow>());
+            }
+        }
+    }
+
 }

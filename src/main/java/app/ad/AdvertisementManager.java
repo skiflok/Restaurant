a3baf7adfbf1d6e6f62692dc14957cf78d5b5363
skiflok <anisimov.a.v@inbox.ru>
2023-03-07 10:39:26 +0300

    getActualAdvertisements () create processVideos() changed. it use getActualAdvertisements now

--- src/main/java/app/ad/AdvertisementManager.java
+++ src/main/java/app/ad/AdvertisementManager.java
@@ -22,34 +22,61 @@ public class AdvertisementManager {
         this.timeSeconds = timeSeconds;
     }
 
-    /*
-    *   задача
-    * подобрать рекламные ролики
-    *   требования
-    * максимальная сумма денег от показа
-    * время показа не превышает время приготовления
-    * нет повторения роликов
-    * если сумма одинаковая то выбрать максимальное время
-    * если время одинаковое то выбрать минимальное количество роликов
-    * */
+    private long maxAmount; // максимальная стоимость роликов
 
-    private long masAmount; // максимальная стоимость роликов
+    private List<Advertisement> optimalPlaylist; // оптимальный плейлист
 
-    private List<Advertisement> optimalPlaylist;
+    private long availableTimeToShow; // доступное время для показа
 
-    private void createOptimalPlayList () {
 
+    /**
+     * подобрать рекламные ролики
+     * требования
+     * максимальная сумма денег от показа
+     * время показа не превышает время приготовления
+     * нет повторения роликов
+     * если сумма одинаковая то выбрать максимальное время
+     * если время одинаковое то выбрать минимальное количество роликов
+     *
+     * @param currentPlayList          текущий плейлист
+     * @param currentAvailableTimeShow оставшееся доступное время для просмотра
+     * @param currentAmount            доход от просмотра
+     */
+    private void createOptimalPlayList(List<Advertisement> currentPlayList,
+                                       int currentAvailableTimeShow,
+                                       long currentAmount) {
+        if (currentAvailableTimeShow < 0) {
+            return;
+        } else if (currentAmount > maxAmount
+                || currentAmount == maxAmount && (availableTimeToShow > currentAvailableTimeShow
+                || availableTimeToShow == currentAvailableTimeShow && currentPlayList.size() < optimalPlaylist.size())) {
+            this.availableTimeToShow = currentAvailableTimeShow;
+            this.maxAmount = currentAmount;
+            this.optimalPlaylist = currentPlayList;
+            if (currentAvailableTimeShow == 0) return;
+        }
+        ArrayList<Advertisement> temp = getActualAdvertisements();
+        temp.removeAll(currentPlayList);
+        for (Advertisement video : temp) {
+            if (!video.isAlive()) continue;
+            ArrayList<Advertisement> currentListTemp = new ArrayList<>(currentPlayList);
+            currentListTemp.add(video);
+            createOptimalPlayList(currentListTemp,
+                    currentAvailableTimeShow - video.getDuration(),
+                    currentAmount + video.getAmountPerOneDisplaying());
+        }
     }
 
 
     /**
      * получить видео с положительным количеством оплаченных показов
+     *
      * @return список видео в доступных для показа
      */
-    private ArrayList<Advertisement> getActualAdvertisements () {
+    private ArrayList<Advertisement> getActualAdvertisements() {
         ArrayList<Advertisement> actualAdvertisements = new ArrayList<>();
         for (Advertisement video : storage.list()) {
-         if (video.isAlive()) actualAdvertisements.add(video);
+            if (video.isAlive()) actualAdvertisements.add(video);
         }
         return actualAdvertisements;
     }
@@ -60,16 +87,16 @@ public class AdvertisementManager {
      * Выполняет сортировку по стоимости за просмотр и по продолжительности
      */
     public void processVideos() {
-        if (storage.list().isEmpty()) {
+        createOptimalPlayList(new ArrayList<>(), timeSeconds, 0L);
+        if (optimalPlaylist == null || optimalPlaylist.isEmpty()) {
             throw new NoVideoAvailableException();
         } else {
-            List<Advertisement> playlist = storage.list();
-            Collections.sort(playlist, (o1, o2) -> {
+            Collections.sort(optimalPlaylist, (o1, o2) -> {
                 long l = o2.getAmountPerOneDisplaying() - o1.getAmountPerOneDisplaying();
                 return (int) (l != 0 ? l : o2.getDuration() - o1.getDuration());
             });
 
-            for (Advertisement video : playlist) {
+            for (Advertisement video : optimalPlaylist) {
                 playVideo(video);
                 video.revalidate();
             }
@@ -81,6 +108,7 @@ public class AdvertisementManager {
      * 1 - название ролика,
      * 2- стоимость показа одного рекламного ролика в копейках,
      * 3 - стоимость показа одной секунды рекламного ролика в тысячных частях копейки.
+     *
      * @param advertisement Рекламный ролик для воспроизведения.
      */
     private void playVideo(Advertisement advertisement) {

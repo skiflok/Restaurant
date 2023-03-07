846cf1f71d43d28f8a53ab038a89535b21c7ac85
skiflok <anisimov.a.v@inbox.ru>
2023-03-07 12:08:14 +0300

    StatisticManager register(EventDataRow data)

--- src/main/java/app/statistic/StatisticManager.java
+++ src/main/java/app/statistic/StatisticManager.java
@@ -1,6 +1,8 @@
 package app.statistic;
 
 
+import app.statistic.event.EventDataRow;
+
 /**
  * Управляет регистрацией событий и сбором статистики
  */
@@ -8,9 +10,14 @@ public class StatisticManager {
 
     private static final StatisticManager instance = new StatisticManager();
 
-    private StatisticManager() {    }
+    private StatisticManager() {
+    }
 
     public static StatisticManager getInstance() {
         return instance;
+    }
+
+    void register(EventDataRow data) {
+        //TODO
     }
 }

945d89f5d93a5d306d41da1cccbe3e97af21412a
skiflok <anisimov.a.v@inbox.ru>
2023-03-07 12:19:02 +0300

    VideoSelectedEventDataRow int totalDuration and constructor

--- src/main/java/app/statistic/event/VideoSelectedEventDataRow.java
+++ src/main/java/app/statistic/event/VideoSelectedEventDataRow.java
@@ -1,4 +1,9 @@
 package app.statistic.event;
 
 public class VideoSelectedEventDataRow {
+    int totalDuration;
+
+    public VideoSelectedEventDataRow(int totalDuration) {
+        this.totalDuration = totalDuration;
+    }
 }

bacf115ce96174d991abc35bfcd1bc24f5e4d6c7
skiflok <anisimov.a.v@inbox.ru>
2023-03-09 09:59:47 +0300

    admin tablet

---
+++ src/main/java/app/DirectorTablet.java
@@ -0,0 +1,31 @@
+package app;
+
+public class DirectorTablet {
+
+
+    /**
+     * сумма заработанная на рекламе, сгруппировать по дням
+     */
+    public void printAdvertisementProfit() {
+    }
+
+    /**
+     * загрузка (рабочее время) повара, сгруппировать по дням
+     */
+    public void printCookWorkLoading() {
+    }
+
+
+    /**
+     * список активных роликов и оставшееся количество показов по каждому
+     */
+    public void printActiveVideoSet() {
+    }
+
+    /**
+     * список НЕ активных роликов (с оставшемся количеством показов равным нулю)
+     */
+    public void printArchivedVideoSet() {
+    }
+
+}

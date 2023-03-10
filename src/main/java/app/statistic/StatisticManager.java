891de8dc33ab3e82c13741e780e32207f0551ea3
skiflok <anisimov.a.v@inbox.ru>
2023-03-10 11:24:15 +0300

    getCookWorkLoading()

--- src/main/java/app/statistic/StatisticManager.java
+++ src/main/java/app/statistic/StatisticManager.java
@@ -1,6 +1,7 @@
 package app.statistic;
 
 
+import app.statistic.event.CookedOrderEventDataRow;
 import app.statistic.event.EventDataRow;
 import app.statistic.event.EventType;
 import app.statistic.event.VideoSelectedEventDataRow;
@@ -32,15 +33,15 @@ public class StatisticManager {
 
     /**
      * Формирует мап {дата - суммарный доход от просмотра видео в этот день}.
+     *
      * @return мап {дата - суммарный доход от просмотра видео в этот день}
      */
     public Map<String, Long> getAdvertisementProfit() {
         Map<String, Long> profitMap = new HashMap<>();
         List<EventDataRow> eventDataRows = statisticStorage.storage.get(EventType.SELECTED_VIDEOS);
-
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
         for (EventDataRow row : eventDataRows) {
             VideoSelectedEventDataRow event = ((VideoSelectedEventDataRow) row);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
             String date = dateFormat.format(event.getCurrentDate());
             if (!profitMap.containsKey(date)) {
                 profitMap.put(date, event.getAmount());
@@ -51,10 +52,31 @@ public class StatisticManager {
         return profitMap;
     }
 
+    public Map<String, Map<String, Integer>> getCookWorkLoading() {
+        Map<String, Map<String, Integer>> res = new HashMap<>();
+        List<EventDataRow> eventDataRows = statisticStorage.storage.get(EventType.COOKED_ORDER);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+        for (EventDataRow row : eventDataRows) {
+            CookedOrderEventDataRow event = ((CookedOrderEventDataRow) row);
+            String date = dateFormat.format(event.getCurrentDate());
+            if (!res.containsKey(date)) {
+                res.put(date, new HashMap<>());
+            }
+            String cookName = event.getCookName();
+            if (res.get(date).containsKey(cookName)) {
+                res.get(date).put(cookName, res.get(date).get(cookName) + event.getCookingTimeSeconds());
+            } else {
+                res.get(date).put(cookName, event.getCookingTimeSeconds());
+            }
+        }
+        return res;
+    }
+
 
     private static class StatisticStorage {
 
         private final Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
         /**
          * Инициализирует хранилище с типами событий и пустым листом
          */
@@ -69,7 +91,6 @@ public class StatisticManager {
         }
 
     }
-
 
 
 }

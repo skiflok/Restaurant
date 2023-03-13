e8dcb1dc0b224dbf36b68809d906a336a6c226c6
skiflok <anisimov.a.v@inbox.ru>
2023-03-13 11:36:17 +0300

    for testing

---
+++ src/main/java/app/RandomOrderGeneratorTask.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.util.List;
+
+public class RandomOrderGeneratorTask implements Runnable {
+
+    private List<Tablet> tablets;
+    private int interval;
+
+    public RandomOrderGeneratorTask(List<Tablet> tablets, int interval) {
+        this.tablets = tablets;
+    }
+
+
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+             int tablet = (int) (Math.random() * tablets.size());
+             Tablet tableGeneratesAnOrder = tablets.get(tablet);
+             tableGeneratesAnOrder.createOrder();
+             Thread.sleep(interval);
+            }
+        } catch (InterruptedException ignored) {
+        }
+    }
+}

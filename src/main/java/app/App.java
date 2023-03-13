6781d055a9d0145f8f99366dc27387bb39a8f69a
skiflok <anisimov.a.v@inbox.ru>
2023-03-13 09:31:43 +0300

    test

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -21,15 +21,21 @@ public class App {
         ConsoleHelper.writeMessage("it's a restaurant Restaurant");
 
         Tablet tablet1 = new Tablet(1);
+        Tablet tablet2 = new Tablet(2);
         Cook cook = new Cook("Cook_1");
+        Cook cook2 = new Cook("Cook_2");
         tablet1.addObserver(cook);
+        tablet2.addObserver(cook2);
         Waiter waiter = new Waiter();
         cook.addObserver(waiter);
+        cook2.addObserver(waiter);
 
         for (int i = 0; i < 1; ++i) {
             tablet1.createOrder();
             is.reset();
         }
+        tablet2.createOrder();
+
         System.setIn(sysInBackup);
 
         ConsoleHelper.writeMessage("\n");

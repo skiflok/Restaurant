4711132148536df03f1be9f4eb6361226e306209
skiflok <anisimov.a.v@inbox.ru>
2023-03-13 10:23:54 +0300

    getVideoSet

--- src/main/java/app/ad/StatisticAdvertisementManager.java
+++ src/main/java/app/ad/StatisticAdvertisementManager.java
@@ -1,13 +1,36 @@
 package app.ad;
 
+import app.ConsoleHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StatisticAdvertisementManager {
 
-    private StatisticAdvertisementManager instance = new StatisticAdvertisementManager();
+    private static final StatisticAdvertisementManager instance = new StatisticAdvertisementManager();
+    private final AdvertisementStorage advertisementStorage = AdvertisementStorage.getStorage();
+
 
     private StatisticAdvertisementManager() {
     }
 
-    public StatisticAdvertisementManager getInstance() {
+    public static StatisticAdvertisementManager getInstance() {
         return instance;
     }
+
+    /**
+     * список активных роликов и оставшееся количество показов по каждому
+     */
+    public List<Advertisement> getVideoSet(boolean isActive) {
+        List<Advertisement> videoSet = new ArrayList<>();
+
+        for (Advertisement advertisement : advertisementStorage.list()) {
+            if (!isActive ^ advertisement.isAlive()) {
+                videoSet.add(advertisement);
+            }
+        }
+
+        return videoSet;
+    }
+
 }

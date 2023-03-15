9bf40d201624f1f0293ea6c9222e805547451ba8
skiflok <anisimov.a.v@inbox.ru>
2023-03-15 11:57:08 +0300

    OrderManager passOrderToCook

--- src/main/java/app/kitchen/OrderManager.java
+++ src/main/java/app/kitchen/OrderManager.java
@@ -1,13 +1,52 @@
 package app.kitchen;
 
+import app.ConsoleHelper;
+import app.Tablet;
+import app.statistic.StatisticEventManager;
+
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+
+/**
+ * Обсервер для столиков.
+ */
 public class OrderManager implements Observer {
 
+    private final static Logger logger = Logger.getLogger(OrderManager.class.getName());
     private LinkedBlockingQueue<Order> orders = new LinkedBlockingQueue<>();
 
+    public OrderManager() {
+        logger.log(Level.INFO, getClass().getName());
+        Thread passOrderToCook = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                logger.log(Level.INFO, "Запущен passOrderToCook");
+
+                Set<Cook> cooks = StatisticEventManager.getInstance().getCooks();
+
+                while (true) {
+                    try {
+//                        logger.log(Level.INFO, "Цикл passOrderToCook");
+                        for (Cook cook : cooks) {
+                            if (!cook.isBusy() && !orders.isEmpty()) {
+                                cook.startCookingOrder(orders.take());
+                            }
+                        }
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        logger.log(Level.SEVERE, "InterruptedException");
+                    }
+                }
+            }
+        });
+        passOrderToCook.setDaemon(true);
+        passOrderToCook.start();
+    }
 
     @Override
     public void update(Observable o, Object order) {
